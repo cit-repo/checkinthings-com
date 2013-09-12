e1a5adff3210b0dd8dc4368e38aa5f80329d779f
Pablo del Vecchio <pablo.del-vecchio@project-a.com>
2013-09-12 13:15:05 +0200

    added google analytics

--- alfa/public/js/functions.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-
-    $("#aKill").click(function(){
-        $.post("/session.php",function(data){
-            // if you want you can show some message to user here
-        });
-    });
-})
