2b21ed50bcded1d6791760f5f84091541b212ab1
citftp <citftp@bidimail.com>
2013-09-17 15:44:55 +0200

    added PROD sync for tracking

--- sync/sync.sh
+++ sync/sync.sh
@@ -1,4 +1,11 @@
 cd /var/www/vhosts/checkinthings.com/httpdocs/sync/
 
-/usr/bin/php /var/www/vhosts/checkinthings.com/httpdocs/sync/mysql-to-couchdb.php 0 1 0 2 >> mysql-to-couchdb.log
+# Customer
+/usr/bin/php /var/www/vhosts/checkinthings.com/httpdocs/sync/mysql-to-couchdb.php customer 0 1 0 2 >> mysql-to-couchdb.log
 /usr/bin/php /var/www/vhosts/checkinthings.com/httpdocs/sync/couchdb-to-mysql.php customer >> couchdb-to-mysql.log
+
+# Product
+/usr/bin/php /var/www/vhosts/checkinthings.com/httpdocs/sync/mysql-to-couchdb.php product 0 1 0 2 >> mysql-to-couchdb.log
+
+# Track
+/usr/bin/php /var/www/vhosts/checkinthings.com/httpdocs/sync/couchdb-to-mysql.php track >> couchdb-to-mysql.log
