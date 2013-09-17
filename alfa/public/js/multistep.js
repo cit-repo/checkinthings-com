0d7aff55f22a514e2ac5aab05c25ca94d0a2312a
Pablo del Vecchio <pablo.del-vecchio@project-a.com>
2013-09-17 19:43:57 +0200

    modified order on registration fields

--- alfa/public/js/multistep.js
+++ alfa/public/js/multistep.js
@@ -2,28 +2,24 @@ $(function(){
     //original field values
     var field_values = {
         //id        :  value
-        'username'  : 'username',
+        'email'  : 'email address',
         'password'  : 'password',
         'cpassword' : 'password',
         'firstname'  : 'first name',
-        'lastname'  : 'last name',
-        'email'  : 'email address'
+        'lastname'  : 'last name'
+
     };
 
-
     //inputfocus
-    $('input#username').inputfocus({ value: field_values['username'] });
+    $('input#email').inputfocus({ value: field_values['email'] });
     $('input#password').inputfocus({ value: field_values['password'] });
     $('input#cpassword').inputfocus({ value: field_values['cpassword'] });
     $('input#lastname').inputfocus({ value: field_values['lastname'] });
     $('input#firstname').inputfocus({ value: field_values['firstname'] });
-    $('input#email').inputfocus({ value: field_values['email'] });
-
 
     //reset progress bar
     $('#progress').css('width','0');
     $('#progress_text').html('0% Complete');
-
 
     //first_step
     $('#submit_first').click(function(){
@@ -31,11 +27,12 @@ $(function(){
         $('#first_step input').removeClass('error').removeClass('valid');
 
         //ckeck if inputs aren't empty
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         var fields = $('#first_step input[type=text], #first_step input[type=password]');
         var error = 0;
         fields.each(function(){
             var value = $(this).val();
-            if( value.length<4 || value==field_values[$(this).attr('id')] ) {
+            if( (value.length<4 || value==field_values[$(this).attr('id')]) || ( $(this).attr('id')=='email' && !emailPattern.test(value) )) {
                 $(this).addClass('error');
                 $(this).effect("shake", { times:3 }, 50);
 
@@ -46,7 +43,7 @@ $(function(){
         });
 
         if(!error) {
-            if( $('#password').val() != $('#cpassword').val() ) {
+            if( $('#password').val() != $('#cpassword').val()) {
                 $('#first_step input[type=password]').each(function(){
                     $(this).removeClass('valid').addClass('error');
                     $(this).effect("shake", { times:3 }, 50);
@@ -70,12 +67,11 @@ $(function(){
         //remove classes
         $('#second_step input').removeClass('error').removeClass('valid');
 
-        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         var fields = $('#second_step input[type=text]');
         var error = 0;
         fields.each(function(){
             var value = $(this).val();
-            if( value.length<1 || value==field_values[$(this).attr('id')] || ( $(this).attr('id')=='email' && !emailPattern.test(value) ) ) {
+            if( value.length<1 || value==field_values[$(this).attr('id')]) {
                 $(this).addClass('error');
                 $(this).effect("shake", { times:3 }, 50);
 
@@ -105,9 +101,8 @@ $(function(){
 
         //prepare the fourth step
         var fields = new Array(
-            $('#username').val(),
-            $('#password').val(),
             $('#email').val(),
+            $('#password').val(),
             $('#firstname').val() + ' ' + $('#lastname').val(),
             $('#age').val(),
             $('#gender').val(),
