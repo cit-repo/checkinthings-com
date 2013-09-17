55c89713d9411b4db2ea2ebd3025ef195f37edd9
Pablo del Vecchio <pablo.del-vecchio@project-a.com>
2013-09-17 15:31:00 +0200

    added basic tracking to backend

--- sync/couchdb-to-mysql.php
+++ sync/couchdb-to-mysql.php
@@ -21,69 +21,111 @@
 
     // print_r($allDocs['rows']);
 
-    foreach ($allDocs['rows'] as $doc) {
-        $doc = $couchdb->readDocument($doc['id']);
+    switch ($mysql_options['table']) {
+        case "track":
+            foreach ($allDocs['rows'] as $doc) {
+                $doc = $couchdb->readDocument($doc['id']);
 
-        $doc = json_decode($doc, true);
+                $doc = json_decode($doc, true);
 
-        $sel = "SELECT * FROM admin_cit.".$mysql_options['table']." WHERE email = '".$doc['email']."';";
-        // echo $sel."\n";
+                $sel = "SELECT * FROM admin_cit.".$mysql_options['table']." WHERE uuid = '".$doc['_id']."';";
+                // echo $sel."\n";
 
-        $res = mysqli_query($link, $sel);
+                $res = mysqli_query($link, $sel);
 
-        if (mysqli_num_rows($res) > 0) {
-            $row = mysqli_fetch_assoc($res);
+                if (mysqli_num_rows($res) > 0) {
 
-            if ($doc['_id'] != $row['uuid']) {
-                $upd = "UPDATE admin_cit.".$mysql_options['table']." SET uuid='".$doc['_id']."' WHERE email = '".$doc['email']."';";
-                echo date('Y-m-d H:i:s')." - ".$upd."\n";
-                $res = mysqli_query($link, $upd);
+                } else {
+                    $ins = "INSERT INTO admin_cit.".$mysql_options['table']." (event, link, http_referer, user_agent, remote_addr, session_id, cookie_id, uuid)
+                            VALUES
+                            ('".$doc['event']."','".$doc['link']."','".$doc['http_referer']."','".$doc['user_agent']."','".$doc['remote_addr']."','".$doc['session_id']."','".$doc['cookie_id']."','".$doc['_id']."');";
+                    echo date('Y-m-d H:i:s')." - ".$ins."\n";
+                    $res = mysqli_query($link, $ins);
+
+                    $sel = "SELECT * FROM admin_cit.".$mysql_options['table']." WHERE uuid = '".$doc['_id']."';";
+                    // echo $sel."\n";
+
+                    $res = mysqli_query($link, $sel);
+                    $row = mysqli_fetch_assoc($res);
+
+                    $doc[$mysql_options['table'].'_id'] = mysqli_insert_id($link);
+                    $doc['last_updated'] = $row['last_updated'];
+
+                    $ret = $couchdb->updateDocument($doc['_id'], $doc);
+
+                    // var_dump($ret);
+                }
+            }
+            break;
+
+        case "customer":
+            foreach ($allDocs['rows'] as $doc) {
+                $doc = $couchdb->readDocument($doc['id']);
+
+                $doc = json_decode($doc, true);
+
+                $sel = "SELECT * FROM admin_cit.".$mysql_options['table']." WHERE email = '".$doc['email']."';";
+                // echo $sel."\n";
+
+                $res = mysqli_query($link, $sel);
+
+                if (mysqli_num_rows($res) > 0) {
+                    $row = mysqli_fetch_assoc($res);
+
+                    if ($doc['_id'] != $row['uuid']) {
+                        $upd = "UPDATE admin_cit.".$mysql_options['table']." SET uuid='".$doc['_id']."' WHERE email = '".$doc['email']."';";
+                        echo date('Y-m-d H:i:s')." - ".$upd."\n";
+                        $res = mysqli_query($link, $upd);
+                    }
+
+                    if ($doc['firstname'] != $row['firstname']) {
+                        $upd = "UPDATE admin_cit.".$mysql_options['table']." SET firstname='".$doc['firstname']."' WHERE email = '".$doc['email']."';";
+                        echo date('Y-m-d H:i:s')." - ".$upd."\n";
+                        $res = mysqli_query($link, $upd);
+                    }
+
+                    if ($doc['lastname'] != $row['lastname']) {
+                        $upd = "UPDATE admin_cit.".$mysql_options['table']." SET lastname='".$doc['lastname']."' WHERE email = '".$doc['email']."';";
+                        echo date('Y-m-d H:i:s')." - ".$upd."\n";
+                        $res = mysqli_query($link, $upd);
+                    }
+
+                    if ($doc['password'] != $row['password']) {
+                        $upd = "UPDATE admin_cit.".$mysql_options['table']." SET password='".$doc['password']."' WHERE email = '".$doc['email']."';";
+                        echo date('Y-m-d H:i:s')." - ".$upd."\n";
+                        $res = mysqli_query($link, $upd);
+                    }
+
+                    if ($doc['last_updated'] != $row['last_updated']) {
+                        $upd = "UPDATE admin_cit.".$mysql_options['table']." SET last_updated='".$doc['last_updated']."' WHERE email = '".$doc['email']."';";
+                        echo date('Y-m-d H:i:s')." - ".$upd."\n";
+                        $res = mysqli_query($link, $upd);
+                    }
+
+                } else {
+                    $ins = "INSERT INTO admin_cit.".$mysql_options['table']." (firstname, lastname, email, password, uuid) VALUES ('".$doc['firstname']."','".$doc['lastname']."','".$doc['email']."','".$doc['password']."','".$doc['_id']."');";
+                    echo date('Y-m-d H:i:s')." - ".$ins."\n";
+                    $res = mysqli_query($link, $ins);
+
+                    $sel = "SELECT * FROM admin_cit.".$mysql_options['table']." WHERE email = '".$doc['email']."';";
+                    // echo $sel."\n";
+
+                    $res = mysqli_query($link, $sel);
+                    $row = mysqli_fetch_assoc($res);
+
+                    $doc[$mysql_options['table'].'_id'] = mysqli_insert_id($link);
+                    $doc['last_updated'] = $row['last_updated'];
+
+                    $ret = $couchdb->updateDocument($doc['_id'], $doc);
+
+                    // var_dump($ret);
+                }
             }
 
-            if ($doc['firstname'] != $row['firstname']) {
-                $upd = "UPDATE admin_cit.".$mysql_options['table']." SET firstname='".$doc['firstname']."' WHERE email = '".$doc['email']."';";
-                echo date('Y-m-d H:i:s')." - ".$upd."\n";
-                $res = mysqli_query($link, $upd);
-            }
-
-            if ($doc['lastname'] != $row['lastname']) {
-                $upd = "UPDATE admin_cit.".$mysql_options['table']." SET lastname='".$doc['lastname']."' WHERE email = '".$doc['email']."';";
-                echo date('Y-m-d H:i:s')." - ".$upd."\n";
-                $res = mysqli_query($link, $upd);
-            }
-
-            if ($doc['password'] != $row['password']) {
-                $upd = "UPDATE admin_cit.".$mysql_options['table']." SET password='".$doc['password']."' WHERE email = '".$doc['email']."';";
-                echo date('Y-m-d H:i:s')." - ".$upd."\n";
-                $res = mysqli_query($link, $upd);
-            }
-
-            if ($doc['last_updated'] != $row['last_updated']) {
-                $upd = "UPDATE admin_cit.".$mysql_options['table']." SET last_updated='".$doc['last_updated']."' WHERE email = '".$doc['email']."';";
-                echo date('Y-m-d H:i:s')." - ".$upd."\n";
-                $res = mysqli_query($link, $upd);
-            }
-
-        } else {
-            $ins = "INSERT INTO admin_cit.".$mysql_options['table']." (firstname, lastname, email, password, uuid) VALUES ('".$doc['firstname']."','".$doc['lastname']."','".$doc['email']."','".$doc['password']."','".$doc['_id']."');";
-            echo date('Y-m-d H:i:s')." - ".$ins."\n";
-            $res = mysqli_query($link, $ins);
-
-            $sel = "SELECT * FROM admin_cit.".$mysql_options['table']." WHERE email = '".$doc['email']."';";
-            // echo $sel."\n";
-
-            $res = mysqli_query($link, $sel);
-            $row = mysqli_fetch_assoc($res);
-
-            $doc[$mysql_options['table'].'_id'] = mysqli_insert_id($link);
-            $doc['last_updated'] = $row['last_updated'];
-
-            $ret = $couchdb->updateDocument($doc['_id'], $doc);
-
-            // var_dump($ret);
-        }
-
+            break;
     }
+
+
 
 
 ?>
