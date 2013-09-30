7dd1041592bd65d8f9ea531a02368f6fba4b6664
Pablo del Vecchio <pablo.del-vecchio@project-a.com>
2013-10-01 00:57:31 +0200

    facebook login

--- alfa/public/js/facebook.js
+++ alfa/public/js/facebook.js
@@ -36,8 +36,6 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
         // dialog right after they log in to Facebook.
         // The same caveats as above apply to the FB.login() call here.
         FB.login();
-
-        facebookTrack('facebookLogin', JSON.stringify(response));
     }
 });
 };
@@ -59,6 +57,7 @@ function testAPI() {
         // console.log("apiMe: "+response);
         // document.getElementById('fb_data').innerHTML = '<br>response: '+JSON.stringify(response);
 
+        facebookTrack('facebookLogin', JSON.stringify(response));
         // console.log('Good to see you, ' + response.name + '.');
     });
 
